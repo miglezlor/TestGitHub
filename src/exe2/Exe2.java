7cc0855a7c21b643b4583b1c78a55adda8962386
mgonzalezlorenzo <mgonzalezlorenzo@atanasoff06.danielcastelao.org>
2015-10-01 08:42:02 +0200

    Inicial

---
+++ src/exe2/Exe2.java
@@ -0,0 +1,22 @@
+
+package exe2;
+
+
+public class Exe2 {
+   static int num2;
+
+
+    public static void main(String[] args) {
+
+        int valor= 3, resultado;
+        resultado = valor ++ ;
+        System.out.println("valor:"+ valor + "\n resultado: "+ resultado);
+        
+        
+        
+        
+        
+        
+    }
+    
+}
